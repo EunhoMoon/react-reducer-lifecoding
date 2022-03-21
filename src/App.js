9f9c39409c318307b61efabb21f8595a9aab3fa7
EunhoMoon <eunho.moon@gmail.com>
2022-03-21 15:21:54 +0900

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
+import { createStore } from 'redux';
+import { Provider, useSelector, useDispatch, connect } from 'react-redux';
+
+const reducer = (currentState, action) => {
+  if (currentState === undefined) {
+    return { number: 1 };
+  }
+  const newState = { ...currentState };
+  // 데이터의 불변성을 유지하기 위해 새로운 state를 생성한다.
+  if (action.type === 'PLUS') {
+    newState.number++;
+  }
+  return newState;
+};
+
+const store = createStore(reducer);
 
 export default function App() {
   return (
     <div id="container">
       <h1>Root</h1>
       <div id="grid">
-        <Left1 />
-        <Right1 />
+        <Provider store={store}>
+          <Left1 />
+          <Right1 />
+        </Provider>
       </div>
     </div>
   );
@@ -23,6 +41,7 @@ const Left1 = () => {
 };
 
 const Left2 = () => {
+  console.log('2');
   return (
     <div>
       <h1>Left2</h1>
@@ -32,9 +51,11 @@ const Left2 = () => {
 };
 
 const Left3 = () => {
+  const number = useSelector((state) => state.number);
+  console.log('3');
   return (
     <div>
-      <h1>Left3</h1>
+      <h1>Left3 : {number}</h1>
     </div>
   );
 };
@@ -58,9 +79,17 @@ const Right2 = () => {
 };
 
 const Right3 = () => {
+  const dispatch = useDispatch();
   return (
     <div>
       <h1>Right3</h1>
+      <input
+        type="button"
+        value="+"
+        onClick={() => {
+          dispatch({ type: 'PLUS' });
+        }}
+      />
     </div>
   );
 };
